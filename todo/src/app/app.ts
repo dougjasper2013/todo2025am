8bd10f79a837b405c9f0a62c0295330abacbaa3d
Douglas Jasper <doug.jasper@trios.com>
2025-06-24 11:04:01 -0400

    June 24th

--- todo/src/app/app.ts
+++ todo/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { TodoList } from './todoList';
 import { TodoItem } from './todoItem';
-
+import { FormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  standalone: false,
+  standalone: true,
+  imports: [FormsModule,
+    MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule,
+    MatTableModule, MatCheckboxModule, MatFormFieldModule, MatInputModule,
+    MatSlideToggleModule
+  ],
   styleUrl: './app.css'
 })
 export class App {
@@ -25,6 +39,16 @@ export class App {
 
   get itemCount(): number{
     return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => !item.complete);
+  }
+  
+  addItem(newItem: string) {
+    if (newItem != "") {
+      this.list.addItem(newItem);
+    }    
   }
 
 }
